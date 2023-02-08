40117c11f4fe7930f24538584c91dd8e2fc60cac
nasser <namini40@gmail.com>
2023-02-08 13:29:41 +0330

    update example

--- example/ios/AdtraceExampleTests/AdtraceExampleTests.m
+++ example/ios/AdtraceExampleTests/AdtraceExampleTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AdjustExampleTests : XCTestCase
+@interface AdtraceExampleTests : XCTestCase
 
 @end
 
-@implementation AdjustExampleTests
+@implementation AdtraceExampleTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
